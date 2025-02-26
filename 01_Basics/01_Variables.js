0fd8b2bd56551eef68ec391f3c04470313055ae5
Aniketp7068 <patilanny7068@gmail.com>
2025-02-26 07:49:45 +0000

    #

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 2345;
+let accountEmail = "aniket@google.com";
+var accountPassword = 2445;
+accountCity = "Kolhapur";
+
+// accountId = 9 // not allowed to change constant
+// console.log(accountId);
+accountEmail = "ap@ap.com";
+accountPassword = 517517;
+accountCity = "Pune";
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
