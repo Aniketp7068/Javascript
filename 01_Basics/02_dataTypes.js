7e78eed13cbe231e3ebde59b27ef42f6d8ef999f
Aniketp7068 <patilanny7068@gmail.com>
2025-02-26 11:10:59 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs not browser
+
+let name = "Aniket";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to power  53
+// bigInt
+// string => ""
+// boolean = > true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "aniket");
+console.log(typeof null);
+console.log(typeof undefined);
