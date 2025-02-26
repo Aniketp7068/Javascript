9c32b10e0b28ac20f1eede544dc5b180669a52a5
Aniketp7068 <patilanny7068@gmail.com>
2025-02-26 13:19:15 +0000

    Conversion of datatypes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,4 +1,40 @@
-let score = 33;
+// var score = "33"
+// var score = "33abc";
+var score = "aniket";
 
-console.log(typeof score);
-console.log(typeof score);
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+// conversion example
+/*
+"33" => 33
+"33abc" => NaN (not a number)
+null => 0
+undefined => NaN
+true => 1
+false => 0
+*/
+let isLoggedIn = 99
+let valueInBoolean = Boolean(isLoggedIn)
+// console.log(valueInBoolean);
+
+// Boolean example
+/*
+1 => true
+0 => false
+"aniket" => true
+"" => false
+99 => true
+*/
+
+let someNumber = 55
+console.log(typeof someNumber);
+
+
+let convertInString = String(someNumber)
+console.log(convertInString);
+console.log(typeof convertInString);
