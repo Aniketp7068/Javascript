7112eb1683d390b2b26b10a919a4aa5224d8b41e
Aniketp7068 <patilanny7068@gmail.com>
2025-02-26 10:15:55 +0000

    #

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -8,9 +8,10 @@ accountCity = "Kolhapur";
 accountEmail = "ap@ap.com";
 accountPassword = 517517;
 accountCity = "Pune";
+let accountState
 /*
 prefer not to use var 
 because of issue in block scope and functional scope
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
