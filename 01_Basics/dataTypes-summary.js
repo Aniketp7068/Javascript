0b6b02620f943068de0abe4eba359ec775af1fa7
Aniketp7068 <patilanny7068@gmail.com>
2025-02-27 08:49:27 +0000

    datatype summary

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,50 @@
+// There are 2 types of dataTypes in JS
+// Primitve
+/*
+1. string
+2. number
+3. boolean
+4. null
+5. undefined
+6. symbl
+7. bigint
+*/
+
+// Example of primitives
+
+// const name = "Aniket"
+// const age = 22
+// const isLoggedIn = true
+// const outTemperature = null
+// let emailId = undefined
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+// console.log(id == anotherId);
+// console.log(id === anotherId);
+
+
+// Reference type / Non- Primitve
+/*
+1. Array
+2. Object
+3. Function
+*/
+
+// Example of Non-Primitive
+/*
+const number = [1,2,3,4,5]
+const user = {
+name: "Aniket",
+address: "Kop"
+phoneNo: 9022333434
+}
+
+function sum(a,b){
+return a + b
+}
+console.log(sum(4,4))
+
+*/
+// const BigInt = 123456645324598347520n
+// console.log(typeof BigInt);
